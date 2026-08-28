a3cd5b9c3b078715ec0b69d7edda06d81949b2c7
Sonny Le <sonlv@templaza.com>
2026-08-28 09:21:30 +0700

    Move theme to root

--- classes/output/core_renderer.php
+++ classes/output/core_renderer.php
@@ -1,5 +1,5 @@
 <?php
-namespace theme_moon\output;
+namespace moodle-theme_moon\classes\output-theme_moon\classes\output-theme_moon\classes\output;
 defined('MOODLE_INTERNAL') || die;
 class core_renderer extends \theme_boost\output\core_renderer {
 }
