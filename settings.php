9c5d04db740f29cc9fb0a49e9b7f21216a6abc03
Sonny Le <sonlv@templaza.com>
2026-08-28 10:32:24 +0700

    Update settings.php

--- settings.php
+++ settings.php
@@ -22,6 +22,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // Redirect to the Moon theme settings page if the 'section' parameter is set to 'themesettingmoon'.
-if (!empty($_GET['section']) && $_GET['section'] === 'themesettingmoon') {
+$section = optional_param('section', '', PARAM_ALPHANUMEXT);
+if ($section === 'themesettingmoon') {
     redirect(new moodle_url('/local/moon/index.php?theme=moon'));
 }
