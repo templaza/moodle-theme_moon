31d47d04f231e00b30cdb56418f9ddd9bb3e5b80
Sonny Le <sonlv@templaza.com>
2026-08-28 10:32:55 +0700

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@ $plugin->component = 'theme_moon';
 $plugin->version   = 2026082800;
 $plugin->requires  = 2022041900;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.0';
+$plugin->release   = '1.0.1';
 $plugin->dependencies = [
     'local_moon' => 2026081800,
 ];
